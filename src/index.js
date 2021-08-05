8a037773966d426cb3449040f8695f5af299cd90
FluxOfPingIntegers <Ryan.M.Schleck@gmail.com>
2021-08-05 15:43:31 -0400

    Done.

--- src/index.js
+++ src/index.js
@@ -4,3 +4,5 @@ const imgUrl = "https://dog.ceo/api/breeds/image/random/4"
 const imgJson = fetch(imgUrl)
 .then(response => response.json())
 .then(data => {return data});
+
+// how do I turn imgJson into an array I can work with??
